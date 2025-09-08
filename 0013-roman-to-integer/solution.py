239525ab1bb6f07370320385991e5d76dda40d86
Varshini Sunkara <varshinisunkara2006@gmail.com>
2025-09-08 03:45:39 +0000

    Sync LeetCode submission Runtime - 8 ms (26.31%), Memory - 17.6 MB (100.00%)

---
+++ 0013-roman-to-integer/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        d = {
+            'I': 1, 'V': 5, 'X': 10,
+            'L': 50, 'C': 100, 'D': 500, 'M': 1000
+        }
+        
+        total = 0
+        n = len(s)
+        i = 0
+        
+        while i < n:
+            # If this numeral is smaller than the next, subtract it
+            if i < n - 1 and d[s[i]] < d[s[i + 1]]:
+                total += d[s[i + 1]] - d[s[i]]
+                i += 2
+            else:
+                total += d[s[i]]
+                i += 1
+        
+        return total
