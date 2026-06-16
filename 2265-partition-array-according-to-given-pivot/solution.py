87db6cdfaf7d6a123ee4dced77b3c6d4100a74aa
Varshini Sunkara <varshinisunkara2006@gmail.com>
2026-06-16 06:16:30 +0000

    Sync LeetCode submission Runtime - 32 ms (50.31%), Memory - 34.1 MB (11.99%)

---
+++ 2265-partition-array-according-to-given-pivot/solution.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        left, right, p = [], [], []
+
+        for num in nums:
+            if num < pivot:
+                left.append(num)
+            elif num == pivot:
+                p.append(num)
+            else:
+                right.append(num)
+
+        return left + p + right
