e09e8a2b924c71f2aae12b03b2c83f8614ddcad3
Varshini Sunkara <varshinisunkara2006@gmail.com>
2026-06-20 06:36:53 +0000

    Sync LeetCode submission Runtime - 3 ms (26.88%), Memory - 19.3 MB (25.03%)

---
+++ 0777-toeplitz-matrix/solution.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        rows, cols = len(matrix), len(matrix[0])
+        
+        for i in range(rows - 1):
+            for j in range(cols - 1):
+                if matrix[i][j] != matrix[i + 1][j + 1]:
+                    return False
+        return True
